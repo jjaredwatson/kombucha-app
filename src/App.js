ae847a558fe445b9ba3828bd0988ebfc7aaf0dad
Jared Watson <jjaredwatson@gmail.com>
2017-09-05 21:28:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <h1>Hello, World</h1>
+    );
+  }
+}
+
+export default Hello;
